54c47e5a1a0bb4a182608a81ce17dc1c5cee509d
nathanstephane <75221936+nathanstephane@users.noreply.github.com>
2023-10-19 16:20:10 +0200

    added bookmarks, kanban, notes, sabbath school, soundscape components

---
+++ src/app/sabbathschool/sabbathschool.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sabbathschool',
+  templateUrl: './sabbathschool.component.html',
+  styleUrls: ['./sabbathschool.component.scss']
+})
+export class SabbathschoolComponent {
+
+}
---
+++ src/app/soundscape/soundscape.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soundscape',
+  templateUrl: './soundscape.component.html',
+  styleUrls: ['./soundscape.component.scss']
+})
+export class SoundscapeComponent {
+
+}
