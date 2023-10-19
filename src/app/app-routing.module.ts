3f0ecbff20be09ae84e3fcd4881769e2f18b9ae5
nathanstephane <75221936+nathanstephane@users.noreply.github.com>
2023-10-19 16:18:45 +0200

    styling the tabs when selected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { KanbanComponent } from './kanban/kanban.component';
+import { SoundscapeComponent } from './soundscape/soundscape.component';
+import { NotesComponent } from './notes/notes.component';
+import { SabbathschoolComponent } from './sabbathschool/sabbathschool.component';
 
 const routes: Routes = [ 
-      {path: 'bookmarks', component: BookmarksComponent}
-  ];
+      {path: 'bookmarks', component: BookmarksComponent},
+      {path: 'kanban', component: KanbanComponent},
+      {path: 'soundscape', component: SoundscapeComponent},
+      {path: 'notes', component: NotesComponent},
+      {path: 'sabbath-school', component: SabbathschoolComponent}
+
+
+    ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
