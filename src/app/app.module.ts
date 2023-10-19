20606931783463abe2c089dcdd91f767ea008ea0
nathanstephane <75221936+nathanstephane@users.noreply.github.com>
2023-10-19 14:31:24 +0200

    setting routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TabsComponent } from './tabs/tabs.component';
+import { BookmarksComponent } from './bookmarks/bookmarks.component';
+import { KanbanComponent } from './kanban/kanban.component';
+import { SoundscapeComponent } from './soundscape/soundscape.component';
+import { NotesComponent } from './notes/notes.component';
+import { SabbathschoolComponent } from './sabbathschool/sabbathschool.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TabsComponent
+    TabsComponent,
+    BookmarksComponent,
+    KanbanComponent,
+    SoundscapeComponent,
+    NotesComponent,
+    SabbathschoolComponent
   ],
   imports: [
     BrowserModule,
